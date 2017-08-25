4af1739b3051c2a42562a1196f46c4a7c10459b0
Lsk08 <745085447@qq.com>
2017-08-25 15:59:07 +0800

    Create EchoServerHandler.java

---
+++ netty/EchoServerHandler.java
@@ -0,0 +1,29 @@
+package com.nettyOfficial;
+
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandler;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+
+/**
+ * Created by yuan on 2017/8/25.
+ */
+public class EchoServerHandler extends ChannelInboundHandlerAdapter {
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        //Echo the message to client
+        System.out.println(msg.toString());
+        ctx.write(msg);
+        ctx.flush();
+    }
+
+    @Override
+    public void channelReadComplete(ChannelHandlerContext ctx) throws Exception {
+        ctx.close();
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        super.exceptionCaught(ctx, cause);
+    }
+}
